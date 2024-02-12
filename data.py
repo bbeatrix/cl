5ac4a2212337214a3d245967f612c235cc5d26fe
bbeatrix <bbea.smile@gmail.com>
2024-02-12 15:12:14 +0000

    Remove unused code from data for now

--- data.py
+++ data.py
@@ -11,20 +11,17 @@ from torchvision import datasets, transforms as tfs
 
 @gin.configurable(denylist=['datadir', 'dataloader_kwargs'])
 class Data:
-    TARGET_TYPES = ['supervised', 'supervised with forgetstats', 'supervised with replay',
-                    'supervised contrastive', 'unsupervised contrastive', 'supcon with simpreserving',
-                    'supcon with interpolation']
-
-    TASKS_SPLIT_TYPES = ["cl", "forgetstatbased", "cl_forgetstatbased", "random"]
-    TASKS_ORDER = ["forgettables_first", "unforgettables_first"]
+    TARGET_TYPES = ['supervised']
+    TASKS_SPLIT_TYPES = ["cl"]
+    TASKS_ORDER = ["default_class_order"]
     INCREMENTAL_LEARNING_SETUP = ["CIL", "TIL"]
 
     def __init__(self, datadir, dataloader_kwargs, dataset_name='cifar10', image_size=32, batch_size=64,
-                 target_type='supervised contrastive', augment=True, num_tasks=1, num_cycles=1,
+                 target_type='supervised', augment=True, num_tasks=1, num_cycles=1,
                  apply_vit_transforms=False, simple_augmentation=False, normalization=False,
-                 tasks_split_type="cl", forgetstats_path=None, tasks_order="forgettables_first", 
-                 randomsubset_task_datasets=False, randomsubsets_size=5000, use_testset_for_training=False,
+                 tasks_split_type="cl", tasks_order="default_class_order",
                  incremental_learning_setup="CIL", validation_ratio=0.0):
+
         err_message = "Data target type must be element of {}".format(self.TARGET_TYPES)
         assert (target_type in self.TARGET_TYPES) == True, err_message
         err_message = "Tasks' split type must be element of {}".format(self.TASKS_SPLIT_TYPES)
@@ -47,30 +44,24 @@ class Data:
         self.simple_augmentation = simple_augmentation
         self.normalization = normalization
         self.tasks_split_type = tasks_split_type
-        self.forgetstats_path = forgetstats_path
         self.tasks_order = tasks_order
-        self.randomsubset_task_datasets = randomsubset_task_datasets
-        self.randomsubsets_size = randomsubsets_size
-        self.use_testset_for_training = use_testset_for_training
         self.incremental_learning_setup = incremental_learning_setup
         self.validation_ratio = validation_ratio
 
         self._setup()
 
-
     @property
     def loaders(self):
         return {'train_loaders': self.train_loaders, 'test_loaders': self.test_loaders, 'valid_loaders': self.valid_loaders}
 
-
     def _setup(self):
         self._get_dataset()
         if os.path.isfile(
-            f"{self.datadir}/{self.dataset_name}_{self.num_tasks}tasks_splittype={self.tasks_split_type}_randomsubsettasks={self.randomsubset_task_datasets}_validationratio={self.validation_ratio}_data.pkl"
+            f"{self.datadir}/{self.dataset_name}_{self.num_tasks}tasks_splittype={self.tasks_split_type}_validationratio={self.validation_ratio}_data.pkl"
         ):
             logging.info(f"Loading {self.dataset_name} {self.num_tasks} data from pickle.")
             with open(
-                f"{self.datadir}/{self.dataset_name}_{self.num_tasks}tasks_splittype={self.tasks_split_type}_randomsubsettasks={self.randomsubset_task_datasets}_validationratio={self.validation_ratio}_data.pkl",
+                f"{self.datadir}/{self.dataset_name}_{self.num_tasks}tasks_splittype={self.tasks_split_type}_validationratio={self.validation_ratio}_data.pkl",
                 "rb",
             ) as f:
                 self.train_task_datasets = pickle.load(f)
@@ -90,7 +81,6 @@ class Data:
         else:
             self._create_tasks()
         self._create_loaders()
-
 
     def _get_dataset(self):
         logging.info(f"Loading {self.dataset_name} dataset from {self.datadir}.")
@@ -172,7 +162,6 @@ class Data:
                 self.inverse_normalize = tfs.Normalize(mean=(-0.4729/0.2833, -0.4487/0.2752, -0.4030/0.2886),
                                                        std=[1/0.2833, 1/0.2752, 1/0.2886])
 
-            import pickle
             train_in = open(self.datadir + "/miniimagenet/mini-imagenet-cache-train.pkl", "rb")
             train = pickle.load(train_in)
             train_x = train["image_data"].reshape([64, 600, 84, 84, 3])
@@ -210,61 +199,6 @@ class Data:
         else:
             raise Exception("{} dataset not found!".format(self.dataset_name))
 
-        if self.target_type in ['supervised with forgetstats', 'supervised with replay', 'supervised contrastive']:
-            self.train_dataset = DatasetWIndices(self.train_dataset)
-            self.test_dataset = DatasetWIndices(self.test_dataset)
-
-        if self.use_testset_for_training:
-            logging.info("Using both the train and test set for training, concatenating them now")
-            self.train_dataset = torch.utils.data.ConcatDataset([self.train_dataset, self.test_dataset])
-            logging.info(f"Successfully concatenated train and test sets, new size of train set is: {len(self.train_dataset)}")
-
-        self.control_group = {"unforgettables": {}, "low_forgettables": {}, "high_forgettables": {}}
-        file_path = f"./data/{self.dataset_name}_train_precomputed_fscores_task=0_epoch=200_studysetup_with_labels.npy"
-        try:
-            precomputed_fscores_with_labels = np.load(file_path)
-            for c in range(self.num_classes):
-                class_indices = np.where(precomputed_fscores_with_labels[1] == c)[0]
-                class_scores = precomputed_fscores_with_labels[0][class_indices]
-                unforgettables_indices = np.where(class_scores == 0)[0][:3]
-                forgettables_indices = np.where(class_scores > 0)[0]
-                sorted_forgettables_indices = np.argsort(class_scores[forgettables_indices])
-                low_forgettables_indices = sorted_forgettables_indices[:3]
-                high_forgettables_indices = sorted_forgettables_indices[-3:]
-                
-                self.control_group["unforgettables"][c] = [self.train_dataset[i] for i in class_indices[unforgettables_indices]]
-                self.control_group["low_forgettables"][c] = [self.train_dataset[i] for i in class_indices[low_forgettables_indices]]
-                self.control_group["high_forgettables"][c] = [self.train_dataset[i] for i in class_indices[high_forgettables_indices]]
-        except FileNotFoundError:
-            logging.info(f"The file '{file_path}' does not exist, control group has not been created.")
-
-        self.images_per_targets = {}
-        train_targets = np.array([self.train_dataset[i][1] for i in range(len(self.train_dataset))])
-        for c in range(0, self.num_classes):
-            trainset_filtered_indices = np.where(train_targets == c)[0]
-            class_images = [self.train_dataset[i] for i in trainset_filtered_indices]
-            self.images_per_targets[c] = torch.stack([class_images[i][0] for i in range(len(class_images))])
-        self.full_trainset_loader = torch.utils.data.DataLoader(self.train_dataset,
-                                                           batch_size=self.batch_size,
-                                                           shuffle=False,
-                                                           collate_fn=default_collate,
-                                                           **self.dataloader_kwargs)
-
-    def _create_randomsubset_task_datasets(self):
-        logging.info(f"Creating random subsets of size {self.randomsubsets_size} from each training dataset.")
-
-        for idx, ds in enumerate(self.train_task_datasets):
-            err_message =  f"Length of {idx}. task dataset {len(ds)} should be greater than the subset size {self.randomsubsets_size}."
-            assert len(ds) >= self.randomsubsets_size, err_message
-
-            indices_permutation = np.random.permutation(len(ds))
-            subset_indices = indices_permutation[:self.randomsubsets_size]
-
-            train_ds_subset = torch.utils.data.Subset(ds, subset_indices)
-            self.train_task_datasets[idx] = train_ds_subset
-            self.train_task_datasets_indices_in_orig[idx] = self.train_task_datasets_indices_in_orig[idx][subset_indices]
-        return
-
     def _create_random_split_task_datasets(self):
         assert (self.num_tasks == 2), "With the random split 2 tasks are allowed only"
         logging.info(f"Splitting training dataset into {self.num_tasks} random parts.")
@@ -285,49 +219,6 @@ class Data:
         logging.info(f"Number of train tasks: {len(self.train_task_datasets)}")
         return
 
-    def _create_forgetstatbased_split_task_datasets(self):
-        assert (self.num_tasks == 2), "With the forgetstat based split 2 tasks are allowed only"
-        logging.info(f"Splitting training dataset into {self.num_tasks} parts based on forgetting stats.")
-        assert (self.forgetstats_path is not None) == True, "Parameter should be set in config file"
-
-        self.forgetstats_with_labels = np.load(self.forgetstats_path)
-        if self.tasks_split_type == "cl_forgetstatbased":
-            train_datasets_to_split = self.train_task_datasets
-            self.train_task_datasets = []
-            test_datasets_to_split = self.test_task_datasets
-            self.test_task_datasets = []
-            self.num_tasks *= 2
-            train_task_datasets_indices_in_orig = self.train_task_datasets_indices_in_orig
-            self.train_task_datasets_indices_in_orig = []
-        elif self.tasks_split_type == "forgetstatbased":
-            train_datasets_to_split = [self.train_dataset]
-            test_datasets_to_split = [self.test_dataset]
-            train_task_datasets_indices_in_orig = [list(range(0, len(self.train_dataset)))]
-
-        for idx, train_ds in enumerate(train_datasets_to_split):
-            ds_forgetstats_with_labels = self.forgetstats_with_labels[:, train_task_datasets_indices_in_orig[idx]]
-            unforgettables_selected_indices = np.where(ds_forgetstats_with_labels[0] == 0)[0]
-
-            forgettables_indices = np.where(ds_forgetstats_with_labels[0] > 0)[0]
-            sorted_forgettables_indices = np.argsort(self.forgetstats_with_labels[0][forgettables_indices])
-            forgettables_selected_indices = forgettables_indices[sorted_forgettables_indices]
-
-            train_ds_subset1 = torch.utils.data.Subset(train_ds,
-                                                       forgettables_selected_indices)
-            train_ds_subset2 = torch.utils.data.Subset(train_ds,
-                                                       unforgettables_selected_indices)
-            
-            if self.tasks_order == "forgettables_first":
-                self.train_task_datasets.extend([train_ds_subset1, train_ds_subset2])
-                self.train_task_datasets_indices_in_orig.extend([forgettables_selected_indices, unforgettables_selected_indices])
-            else:
-                self.train_task_datasets.extend([train_ds_subset2, train_ds_subset1])
-                self.train_task_datasets_indices_in_orig.extend([unforgettables_selected_indices, forgettables_selected_indices])
-
-            self.test_task_datasets.extend([test_datasets_to_split[idx], test_datasets_to_split[idx]])
-
-        return
-
     def _create_tasks(self):
         self.train_task_datasets, self.test_task_datasets, self.validation_task_datasets = [], [], []
         self.train_task_datasets_indices_in_orig, self.test_task_datasets_indices_in_orig = [], []
@@ -344,7 +235,7 @@ class Data:
             self.test_task_datasets_indices_in_orig = [list(range(len(self.test_dataset)))]
             self.labels_per_task = [self.labels]
 
-        elif self.num_tasks > 1 and self.tasks_split_type  in ["cl", "cl_forgetstatbased"]:
+        elif self.num_tasks > 1 and self.tasks_split_type in ["cl"]:
             logging.info(f"Splitting training and test datasets into {self.num_tasks} parts for cl.")
             train_targets = [self.train_dataset[i][1] for i in range(len(self.train_dataset))]
             test_targets = [self.test_dataset[i][1] for i in range(len(self.test_dataset))]
@@ -372,15 +263,6 @@ class Data:
                 self.train_task_datasets_indices_in_orig.append(trainset_filtered_indices)
                 self.test_task_datasets_indices_in_orig.append(testset_filtered_indices)
 
-        if self.tasks_split_type in ["forgetstatbased", "cl_forgetstatbased"]:
-            self._create_forgetstatbased_split_task_datasets()
-
-        elif self.tasks_split_type == "random":
-            self._create_random_split_task_datasets()
-
-        if self.randomsubset_task_datasets == True:
-            self._create_randomsubset_task_datasets()
-
         if self.validation_ratio > 0.0:
             self._create_validation_task_datasets()
 
@@ -393,12 +275,11 @@ class Data:
 
         logging.info("Getting number of train examples per class per train task dataset")
         for idx, ds in enumerate(self.train_task_datasets):
-            #num_examples_per_class_per_ds = [len([1 for i in range(len(ds)) if ds[i][1]==c]) for c in self.labels]
             targets = [ds[i][1] for i in range(len(ds))]
             num_examples_per_class_per_ds = {c: targets.count(c) for c in self.labels}
             logging.info(f"Number of train examples per classes in {idx + 1}. train task: {num_examples_per_class_per_ds}")
         with open(
-            f"{self.datadir}/{self.dataset_name}_{self.num_tasks}tasks_splittype={self.tasks_split_type}_randomsubsettasks={self.randomsubset_task_datasets}_validationratio={self.validation_ratio}_data.pkl",
+            f"{self.datadir}/{self.dataset_name}_{self.num_tasks}tasks_splittype={self.tasks_split_type}_validationratio={self.validation_ratio}_data.pkl",
             "wb",
         ) as file:
             pickle.dump(self.train_task_datasets, file)
@@ -461,18 +342,6 @@ class Data:
         self.train_loaders = self.train_loaders * self.num_cycles
         self.test_loaders = self.test_loaders * self.num_cycles
         self.valid_loaders = self.valid_loaders * self.num_cycles
-
-
-class DatasetWIndices(torch.utils.data.Dataset):
-    def __init__(self, tv_dataset):
-        self.dataset = tv_dataset
-
-    def __getitem__(self, index):
-        data, target = self.dataset[index]
-        return data, target, index
-
-    def __len__(self):
-        return len(self.dataset)
 
 
 class TensorDatasetWTransforms(torch.utils.data.Dataset):
